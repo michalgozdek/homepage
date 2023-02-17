079a27fda7413e394ac15a05bef6371c2c37f64e
michal gozdek <michalgozdek12@gmail.com>
2023-02-17 17:36:55 +0100

    newfunctionsJS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-console.log("cześć");
-const buttonRemove = document.querySelector(".header__button");
-const photo = document.querySelector(".header__img");
+{
+    console.log("cześć");
 
-buttonRemove.addEventListener("click", () => { photo.remove(); });
+    const photoRemove = () => { photo.remove(); }
 
+    const buttonRemove = document.querySelector(".header__button");
+    const photo = document.querySelector(".header__img");
 
-const body = document.querySelector(".body")
-const button = document.querySelector(".button__background")
-const thenName = document.querySelector(".button__element")
+    buttonRemove.addEventListener("click", photoRemove);
 
-button.addEventListener("click", () => {
-    body.classList.toggle("boddy__dark");
+    const onClickChangeBackground = () => {
+        body.classList.toggle("boddy__dark");
+        thenName.innerText = body.classList.contains("boddy__dark") ? "jasny" : "ciemny";
+    }
 
-    thenName.innerText = body.classList.contains("boddy__dark") ? "jasny" : "ciemny";
-});
+    const body = document.querySelector(".body")
+    const button = document.querySelector(".button__background")
+    const thenName = document.querySelector(".button__element")
+
+    button.addEventListener("click", onClickChangeBackground);
+}
